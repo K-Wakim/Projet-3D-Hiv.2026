f25611bc368f642b090d5c1d9eab5bb1218864cb
Samuel Filteau-Hubert <samuelfilteauhubert@gmail.com>
2026-04-16 13:33:42 -0400

    fonction démarrer pour partir le code

--- JS/Main.js
+++ JS/Main.js
@@ -1,1 +1,9 @@
 // Contenu du fichier: matrice 31x31, lancer Webgl, charger images/ sons, creer la scène 3D, appeler les fonctions de dessin, démarrer le jeu, générer la loop principale
+function demarrer() {
+    var canvas = document.getElementById("monCanvas");
+    var gl = initWebGL(canvas);
+
+    var prog = initShaders(gl);
+
+    // À compléter ensuite
+}
